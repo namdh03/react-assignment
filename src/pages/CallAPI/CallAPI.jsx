78ec6babce1b217d662da7daaf1c29310c8e21b1
namdh03 <duonghoangnam@gmail.com>
2023-09-02 20:46:55 +0700

    Create call api table

--- src/pages/CallAPI/CallAPI.jsx
+++ src/pages/CallAPI/CallAPI.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import "./table.css";
 
 const CallAPI = () => {
-    return <div>CallAPI</div>;
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+            .then((response) => response.json())
+            .then((data) => setUsers(data));
+    }, []);
+
+    return (
+        <table className="content-table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user) => (
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
 };
 
 export default CallAPI;
